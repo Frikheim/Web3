45368d3e8f4cc7648f818840b06021ab5f018de8
Frikheim <fredrik.rikheim@outlook.com>
2020-10-31 16:31:45 +0100

    noe ble lagt til

---
+++ Individuell Oppgave/ClientApp/src/app/Spørsmål.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Spørsmål = void 0;
+var Spørsmål = /** @class */ (function () {
+    function Spørsmål() {
+    }
+    return Spørsmål;
+}());
+exports.Spørsmål = Spørsmål;
+//# sourceMappingURL=Spørsmål.js.map
